2215d4e93323782e498b4fe2f0d292b54ddfb487
Martin Goldhahn <mgoldhahn@gmail.com>
2021-03-21 14:15:00 +0100

    Book API seems to have changed, fix test

--- spring-rest-invoker/src/test/java/com/github/ggeorgovassilis/springjsonmapper/integrationtests/AbstractGoogleBooksApiTest.java
+++ spring-rest-invoker/src/test/java/com/github/ggeorgovassilis/springjsonmapper/integrationtests/AbstractGoogleBooksApiTest.java
@@ -3,15 +3,14 @@ package com.github.ggeorgovassilis.springjsonmapper.integrationtests;
 import com.github.ggeorgovassilis.springjsonmapper.services.BookService;
 import com.github.ggeorgovassilis.springjsonmapper.services.Item;
 import com.github.ggeorgovassilis.springjsonmapper.services.QueryResult;
-import com.github.ggeorgovassilis.springjsonmapper.services.VolumeInfo;
 import com.github.ggeorgovassilis.springjsonmapper.spring.SpringRestInvokerProxyFactoryBean;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 
 /**
@@ -30,24 +29,31 @@ public abstract class AbstractGoogleBooksApiTest {
 	public void testFindBooksByTitle() throws Exception {
 
 		QueryResult result = bookService.findBooksByTitle("\"Philosophiae naturalis principia mathematica\"");
-		assertTrue(result.getItems().size() > 0);
-		boolean found = false;
-		for (Item item : result.getItems()) {
-			VolumeInfo info = item.getVolumeInfo();
-
-			found |= info != null && info.getAuthors() != null && !info.getAuthors().isEmpty()
-					&& ("Philosophiae naturalis principia mathematica".equals(info.getTitle())
-							&& "Sir Isaac Newton".equals(info.getAuthors().get(0)));
-		}
-		assertTrue(found);
+		assertThat(result,
+			hasProperty("items",
+				hasItem(hasProperty("volumeInfo",
+					both(
+						hasProperty("title", equalTo("Philosophiae naturalis principia mathematica")))
+						.and(
+							hasProperty("authors", hasItem("Isaac Newton"))
+						)
+					)
+				)
+			)
+		);
 	}
 
 	@Test
 	public void testFindBooksById() {
 		Item item = bookService.findBookById("3h9_GY8v-hgC");
-		VolumeInfo info = item.getVolumeInfo();
-		assertEquals("Philosophiae naturalis principia mathematica", info.getTitle());
-		assertEquals("Isaac Newton", info.getAuthors().get(0));
+		assertThat(item, hasProperty("volumeInfo",
+			both(
+				hasProperty("title", equalTo("Philosophiae naturalis principia mathematica")))
+				.and(
+					hasProperty("authors", hasItem("Isaac Newton"))
+				)
+			)
+		);
 	}
 
 }
